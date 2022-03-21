301623f03368c6142c5d4b2d3b28c0c161063c8a
aurelian <631277930@qq.com>
2022-03-21 08:49:48 +0800

    Third commit

--- veggies.py
+++ veggies.py
@@ -1,4 +1,4 @@
-veg_price = {'cabbage': 2.87, 'carrot': 1.59, 'spinach': 3.33, 'asparagus': 2.54,'artichoke': 3.00, 'lettuce': 2.43}
+veg_price = {'cabbage': 200.87, 'carrot': 1.59, 'spinach': 3.33, 'asparagus': 2.54,'artichoke': 3.00, 'lettuce': 2.43}
 order = {'cabbage': 20, 'artichoke': 5}
 total = 0
 for veggie, quantity in order.items():

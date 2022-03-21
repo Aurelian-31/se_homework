de58f266d7b07892bbc3a884efe7e0dddb504791
aurelian <631277930@qq.com>
2022-03-21 08:43:17 +0800

    Second

--- veggies.py
+++ veggies.py
@@ -1,5 +1,5 @@
 veg_price = {'cabbage': 2.87, 'carrot': 1.59, 'spinach': 3.33, 'asparagus': 2.54,'artichoke': 3.00, 'lettuce': 2.43}
-order = {'cabbage': 2, 'artichoke': 5}
+order = {'cabbage': 20, 'artichoke': 5}
 total = 0
 for veggie, quantity in order.items():
     total += veg_price[veggie] * quantity
